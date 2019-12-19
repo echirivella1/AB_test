22fdbbd8da2faf34aa5951fd78d1467150836e98
Edgar Chirivella <echirivella@okode.com>
2019-12-19 11:23:26 +0100

    Initialize firebase on app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { FirebaseService } from './services/firebase';
 
 @Component({
   selector: 'app-root',
@@ -13,15 +14,20 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private firebase: FirebaseService
   ) {
     this.initializeApp();
   }
 
-  initializeApp() {
+  async initializeApp() {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+
+    if (this.platform.is('hybrid')) {
+      await this.firebase.init();
+    }
   }
 }
