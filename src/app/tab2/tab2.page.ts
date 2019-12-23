f1bca7a8223942c4894c300aa7b08963a95d9be7
Edgar Chirivella <echirivella@okode.com>
2019-12-23 12:29:39 +0100

    Setup AB tests on page 2

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FirebaseService } from '../services/firebase';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
 
-  constructor() {}
+  // Default values for A/B test
+  abTestBigWelcome = false;
+  abTestResourcesBackgroundColor = '#FFF';
+  abTestResourcesLayout = '1';
+
+  constructor(
+    private firebase: FirebaseService
+  ) {}
+
+  ngOnInit() {
+    this.setupAbTest();
+  }
+
+  async setupAbTest() {
+    this.abTestBigWelcome = await this.firebase.getAbTestConfigValue('tabTwo_bigWelcome').toPromise();
+    this.abTestResourcesBackgroundColor = await this.firebase.getAbTestConfigValue('tabTwo_resourcesBackgroundColor').toPromise();
+    this.abTestResourcesLayout = await this.firebase.getAbTestConfigValue('tabTwo_resourcesLayout').toPromise();
+  }
 
 }
