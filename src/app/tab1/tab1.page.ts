26716b5c497b7e3a7510942ef5f352a178ed2235
Edgar Chirivella <echirivella@okode.com>
2019-12-23 12:41:59 +0100

    Change ionViewWillEnter to ngOnInit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,21 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FirebaseService } from '../services/firebase';
-import { of } from 'rxjs';
-import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
   abTestBlue = false; // By default, do not enable a specific AB case
 
   constructor(
     private firebase: FirebaseService
   ) {}
 
-  async ionViewWillEnter() {
+  ngOnInit() {
     this.firebase.getConfigValue('ab_test_tabOne_blue').subscribe(value => this.abTestBlue = value);
   }
 }
